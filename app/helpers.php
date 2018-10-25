881338b41787c0221264c01986c99858c00792ed
Ouhamza <ouhamza.web.pro@gmail.com>
2018-10-25 22:32:48 +0100

    Add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,10 @@ if(! function_exists('page_title')){
     return $title.' | Laracarte Liste of Artisan.';
 }
 }
+
+if(! function_exists('set_active_route')){
+     function set_active_route($route){
+         
+    return Route::is($route) ? 'active' :'';
+}
+}
